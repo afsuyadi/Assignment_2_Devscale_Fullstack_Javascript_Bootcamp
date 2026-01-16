af127af29a0dfd22233f9e0bb25b1901e4afe632
afsuyadi <afsuyadi@gmail.com>
2026-01-16 09:38:33 +0700

    fix bug

--- src/validation/participant-validation.ts
+++ src/validation/participant-validation.ts
@@ -1,12 +1,12 @@
 import z from "zod";
 
-export const createParticipantValiation = z.object({
+export const createParticipantsValidation = z.object({
     name : z.string().min(3),
     email : z.string().email(),
     eventId : z.string(),
 })
 
-export const updateParticipantValiation = z.object({
+export const updateParticipantsValidation = z.object({
     name : z.string().min(3).optional(),
     email : z.string().email().optional(),
     eventId : z.string().min(1).optional(),
